d54a6788aa974b8edca71872110890360a0ebce7
rizkyputri99 <rizkyputripertiwi13@gmail.com>
2023-02-07 11:31:11 +0700

    first commit

---
+++ pages/movies.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Movies({ movie }) {
+  return (
+    <div>
+        
+      <div>
+        <h3 className="font-medium py-3 text-center">{movie.title}</h3>
+        <img
+          src={"https://image.tmdb.org/t/p/w300" + movie.backdrop_path}
+          alt={movie.path}
+        />
+        <h3 className="text-sm py-2">Release date : {movie.release_date}</h3>
+        <h3 className="text-sm line-clamp-3">Overview : <br></br>{movie.overview}</h3>
+      </div>
+    </div>
+
+    
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+      listStyleType: {
+        none: 'none',
+        disc: 'disc',
+        decimal: 'decimal',
+        square: 'square',
+        roman: 'upper-roman',
+      },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+}
